4e6d6d1c96244c65e22716e54622391be4622280
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2022-01-13 08:43:36 -0600

    edit README, removed css, began outline for appjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    baseURL: 'http://jservice.io/api/random',
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.setState({
+
+    }, () => {
+      fetch(this.state.baseURL)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error))
+    })
+  }
+
+  render() {
+    return (
+      <div id="container">
+        <h1>Welcome to Trivia!</h1>
+        <h2>Score: </h2>
+        <div>
+          <h3>Category: </h3>
+          <h4>Points: </h4>
+        </div>
+
+        <div id="answer">
+          <h5>Answer: </h5>
+        </div>
+
+        <button type="submit">What is...?</button>
+      </div> /* closes container */
+    );
+  }
 }
 
 export default App;
