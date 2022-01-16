bda34329a8a092a7eaf51555557b8e9cc6cac9b1
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2022-01-16 12:48:38 -0600

    have score buttons and next question button; answer reveal in console not DOM, scoring tbc

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 //Components
+let element = document.getElementsByClassName("hide");
+function toggle() {
+  element.className.toggle("hide")
+}
+
 
 
 class App extends Component {
@@ -13,6 +18,7 @@ class App extends Component {
     answer: '',
     category: '',
     setQuestion: '',
+    toggle: false,
   }
 
   handleChange = (e) => {
@@ -21,30 +27,29 @@ class App extends Component {
     })
   }
 
-  // handleSubmit = (e) => {
-  //   e.preventDefault()
 
-  //   this.setState({
-  //     data: {
-  //       question: this.state.question,
-  //       answer: this.state.answer,
-  //       category: this.state.category,
-  //     }
-
-  //   }, () => {
-  //     fetch(this.state.baseURL)
-  //       .then(response => response.json())
-  //       .then(data => console.log(data))
-  //       .catch(error => console.error(error))
-  //   })
-  // }
 
   componentDidMount() {
-    // console.log('mounted app.js')
     fetch(this.state.baseURL)
       .then(response => response.json())
       .then(data => this.setState({ setQuestion: data.pop() }))
       .catch(error => console.error(error))
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    this.setState({
+      setQuestion: {
+        // toggle: true,
+      }
+
+    }, () => {
+      fetch(this.state.baseURL)
+        .then(response => response.json())
+        .then(data => this.setState({ setQuestion: data.pop() }))
+        .catch(error => console.error(error))
+    })
   }
 
   render() {
@@ -59,16 +64,38 @@ class App extends Component {
         <div>
           <h1>Welcome to Trivia!</h1>
 
-          <h2>Score: </h2>
-          <h2>Category: {category?.title}</h2>
-          <h4>Points: {value}</h4>
+            <h2>Score: </h2>
+          <div className="score-button">
+            <button id="increase">Increase</button>
+
+            <button id="decrease">Decrease</button>
+
+            <button id="reset">Reset</button>
+
+          </div>
+          <h2>Category: <br />
+            {category?.title}</h2>
+
+          <h4 id="points-box">Points: {value}</h4>
+
           <div id="answer">
-            <h5>Answer: {answer}</h5>
+            <h5>Question:</h5>
+            {question}
+          </div>
+            
+          <div id="question-box" onClick={() => this.setState({toggle: !this.state.toggle})}>
+            Click Here to Reveal the Answer:
+
+              <div className="hide" > 
+                {answer}
+                {console.log({answer})}
+              </div>
           </div>
 
-          <div id="question-box">
-            <h3>What is...?: {question}</h3>
-            </div>
+                <div id="next-question">
+              <button onClick={this.handleSubmit}>Click Here for the Next Question</button>
+              </div>
+
         </div>
       </div>  /* closes container */
     )
